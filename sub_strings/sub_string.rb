bc9e46fe37d58d161058b96f8a4d78063c302c44
Geoffrey MESNIER <mesniergeoffrey@gmail.com>
2020-11-26 18:22:03 +0100

    second modif

--- sub_strings/sub_string.rb
+++ sub_strings/sub_string.rb
@@ -18,8 +18,7 @@
 
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-
 puts(substrings("Howdy partner, sit down! How's it going?",dictionary))
 
-#test
+
 
