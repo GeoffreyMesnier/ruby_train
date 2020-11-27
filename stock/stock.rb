0c4073eb3515c8d25d2fccbe5a0c5a03a99bbef1
Geoffrey MESNIER <mesniergeoffrey@gmail.com>
2020-11-27 09:45:04 +0100

    Final stock version

--- stock/stock.rb
+++ stock/stock.rb
@@ -1,1 +1,21 @@
 #Fonction pour gérer le stock
+def stock_picker(valeurStock)
+	result =[0,0]
+	
+	#Parcours les valeur d'achat
+	valeurStock.each_with_index{|valeurAchat,jourAchat|
+		#Parcours les valeurs de vente
+		valeurStock.each_with_index{|valeurVente,jourVente|
+			if jourVente > jourAchat
+				if (valeurVente - valeurAchat) > (valeurStock[result[1]] - valeurStock[result[0]] )
+					result = [jourAchat,jourVente]
+				end
+			end
+		}
+	}
+	return result
+	#Choisi le max du stock
+end
+
+
+puts(stock_picker([17,3,6,9,15,8,6,1,10]))
